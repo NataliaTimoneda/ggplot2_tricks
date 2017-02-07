26625444bf241629c26058af62c70d35af92a1bb
nuriap <nuriaponsab@gmail.com>
2017-02-07 12:30:24 +0100

    he afegit una suma

--- te.pl
+++ te.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl
+use strict; use warnings;
+
 print "1\t2\t3\t4\t4\5\t6\t7\t8\t9\t10\n";
 print join ("\t", (1..10), "\n");
 
-for $i (1..10){
+for my $i (1..10){
 print "$i\t";
 }
 print "\n";
 
-for ($i=1; $i<=10; $i++){
+for (my $i=1; $i<=10; $i++){
 print "$i\t";
 }
 print "\n";
@@ -15,3 +17,7 @@ print "\n";
 print "saps fer-ho de més maneres?\n";
 
 print "ho sento... necessito uns mesents més... jajajajja\n";
+
+my $x=3;
+my $y=2;
+print "$x plus $y is ", $x + $y, "\n";
