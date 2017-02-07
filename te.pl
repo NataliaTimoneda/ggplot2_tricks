2c6f8cfc857fcd82376c91a80c3b912e96fec3af
alumne <natalia.timoneda@gmail.com>
2017-02-07 11:27:22 +0100

    Afegir el programa te.pl per priemr cop

---
+++ te.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+print "1\t2\t3\t4\t4\5\t6\t7\t8\t9\t10\n";
+print join ("\t", (1..10), "\n");
+
+for $i (1..10){
+print "$i\t";
+}
+print "\n";
+
+for ($i=1; $i<=10; $i++){
+print "$i\t";
+}
+print "\n";
+
+
